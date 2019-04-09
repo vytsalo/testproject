a08bb8390886a2def07cafb6dab673b3a0a0639f
Васильев Виталий Сергеевич <vasilevvs@main.sgu.ru>
2019-04-09 14:28:26 +0400

    фыв

---
+++ src/main/java/config/AppConfig.java
@@ -0,0 +1,37 @@
+package config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+    @Configuration
+    @EnableTransactionManagement
+    @ComponentScans(value = {
+            @ComponentScan("dao"),
+            @ComponentScan("service")
+    })
+    @SuppressWarnings("unused")
+    public class AppConfig {
+
+    @Bean
+    @SuppressWarnings("unused")
+    public LocalEntityManagerFactoryBean getEntityManagerFactoryBean(){
+        LocalEntityManagerFactoryBean factoryBean = new LocalEntityManagerFactoryBean();
+        factoryBean.setPersistenceUnitName("LOCAL_PERSISTENCE");
+        return factoryBean;
+    }
+
+    @Bean
+    @SuppressWarnings("unused")
+    public JpaTransactionManager getJpaTransactionManager(){
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(getEntityManagerFactoryBean().getObject());
+        return transactionManager;
+    }
+
+
+}
