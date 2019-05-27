c3f325237492f6c4b8c4094264591a5161e7e0cf
Васильев Виталий Сергеевич <vasilevvs@main.sgu.ru>
2019-05-27 11:34:12 +0400

    404 all requests

--- src/main/java/controller/HomeController.java
+++ src/main/java/controller/HomeController.java
@@ -7,12 +7,9 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
-
 
 //http://localhost:8009/
 //http://localhost:8082/
@@ -31,24 +28,13 @@ public class HomeController {
         DateFormat dateFormat =
                 DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 
-
         String formattedDate = dateFormat.format(date);
 
         model.addAttribute("ServerTime", formattedDate);
 
         return "home";//вьюшка
-///
     }
 
-
-    /*
-    * по шагам
-    * http://qaru.site/questions/1397251/tomcat-spring-integration-returning-404
-    *
-    *
-    */
-
-    //https://www.journaldev.com/3358/spring-requestmapping-requestparam-pathvariable-example
     @RequestMapping(value = "/user", method = RequestMethod.POST)
     //@Validated who?
     public String posteacher(@Validated User user, Model model) {
@@ -57,7 +43,6 @@ public class HomeController {
         model.addAttribute("userName", user.getUserName());
 
         return "user";//вьюшка
-
     }
 
     @RequestMapping(value = "/lala", method = RequestMethod.GET)
@@ -72,13 +57,10 @@ public class HomeController {
         return "login";
     }
 
-
     //Страница списка всех групп
-
     @RequestMapping(value = "/groupslist", method = RequestMethod.GET)
     public String groups_page() {
         return "main";
     }
-
 
 }
